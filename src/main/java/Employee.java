6a246d2aebaeeded9569de31984b20b9c8ee783f
GrantGit123 <gaguir9@illinois.edu>
2018-03-14 12:47:40 -0500

    Finished methods

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -4,14 +4,18 @@
  * @see <a href="https://cs125.cs.illinois.edu/lab/10/">Lab 10 Description</a>
  */
 public class Employee {
-
+    /**
+     * Name of employee.
+     */
     private String name;
+    /**
+     * Name of manager.
+     */
     private String manager;
-
     /**
      * Constructor for initialization.
-     * @param name
-     * @param manager
+     * @param name name of employee.
+     * @param manager manager name.
      */
     public Employee(final String name, final String manager) {
         this.name = name;
